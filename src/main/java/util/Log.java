c98080991e241e175dd5ce7a6caadf44ee56c0aa
Daniel Melo <danielmelogpi@gmail.com>
2015-01-25 18:34:02 -0200

    Métodos simples para log de erro e info

--- src/main/java/util/Log.java
+++ src/main/java/util/Log.java
@@ -2,11 +2,19 @@ package util;
 public class Log {
 	
 	/** Imprime qualquer quantidade de qualquer trem com um toString() */
-	public static void out(Object... args) {
+	public static void out(String append, Object... args) {
 		for (Object arg : args) {
-	        System.out.print(arg + "\t");
+	        System.out.print(append + arg + "\t");
 	    }
 		System.out.println("");
+	}
+	
+	public static void error(Object... args) {
+		out("ERROR | ", args);
+	}
+	
+	public static void info(Object... args) {
+		out("INFO | ", args);
 	}
 	
 }
