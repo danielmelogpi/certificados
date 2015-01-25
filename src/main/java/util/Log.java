92a54fa8a365f04f05a6e9f1a978e603833d2040
Daniel Melo <danielmelogpi@gmail.com>
2015-01-25 21:58:27 -0200

    Adicionados metodos simples de log

--- src/main/java/util/Log.java
+++ src/main/java/util/Log.java
@@ -1,16 +1,43 @@
 package util;
+
+import java.io.PrintStream;
+
+/** Utilitario simples para logar os experimentos sem adicao de 
+ * biblioteca externa 
+ * 
+ * @author Daniel Melo 
+ * github.com/danielmelogpi
+ *
+ */
 public class Log {
 	
 	/** Imprime qualquer quantidade de qualquer trem com um toString() */
 	public static void out(String append, Object... args) {
-		for (Object arg : args) {
-	        System.out.print(append + arg + "\t");
+		printToStream(System.out, append, args);
+	}
+	
+	public static synchronized final void printToStream(PrintStream stream, String append, Object[] obj) {
+		stream.print(append);
+		for (Object arg : obj) {
+			stream.print(arg + "");
 	    }
-		System.out.println("");
+		stream.println("");
+	}
+	
+	public static void out(Object obj) {
+		out("", obj);
 	}
 	
 	public static void error(Object... args) {
-		out("ERROR | ", args);
+		printToStream(System.out, "ERROR | ", args);
+	}
+	
+	public static void debug(Object... args) {
+		out("DEBUG | ", args);
+	}
+	
+	public static void warn(Object... args) {
+		out("WARN | ", args);
 	}
 	
 	public static void info(Object... args) {
